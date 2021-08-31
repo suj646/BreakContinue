ff48ffdd5ab94f2dbfa74ea5dc56a5ee689539b9
Sujit Bagade <sujitbagade2@gmail.com>
2021-08-31 22:27:12 +0530

    BreakContinue

---
+++ Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace breakcontinue
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            con2 obj1 = new con2();
+            obj1.con1();
+            
+
+            for (int i = 0; i < 10; i++)
+            {
+                if (i == 4)
+                {
+                    break;
+                }
+                Console.WriteLine(i);
+            }
+        }
+    }
+}
+
+class con2
+{
+    public void con1()
+
+    {
+
+        for (int i = 0; i < 10; i++)
+        {
+            if (i == 4)
+            {
+                continue;
+            }
+            Console.WriteLine(i);
+        }
+    }
+
+}
